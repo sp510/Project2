2fe60529599d46845c65d8bfed08f301e2e0ea1e
Steven <Sphan510@gmail.com>
2017-11-06 17:05:40 -0800

    version control 1

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-/*
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hello World!' });
 });
-*/
+
 
 //########################################
 //to process data sent in on request need body-parser module
@@ -19,12 +19,16 @@ router.use(bodyParser.urlencoded({ extended: true })); // for parsing applicatio
 //#########################################
 
 // GET with  URI  /read/Lynne   which means name=Lynne
-router.get('/read/:name', function(req, res, next) {
+router.get('/readNameAndRespond/:name', function(req, res, next) {
     //expecting data variable called name --retrieve value using body-parser
     var body = JSON.stringify(req.body);  //if wanted entire body as JSON
     var params = JSON.stringify(req.params);//if wanted parameters
     var value_name = req.params.name;  //retrieve the data associated with name
-    res.send("hello " + value_name);
+
+    res.render('readNameAndRespond', {outputName: req.params.name })
+
+    //res.send("hello " + value_name);
 })
+
 
 module.exports = router;
