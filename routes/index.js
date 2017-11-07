d50120e4ecebdb996d320b97ec3bd310fbdab257
Steven <Sphan510@gmail.com>
2017-11-07 00:42:35 -0800

    versio

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-/*
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello World!' });
-});
-*/
-
 //########################################
+
 //to process data sent in on request need body-parser module
+
 var bodyParser = require('body-parser');
 var path = require ('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
@@ -18,14 +13,9 @@ router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
 
-
-
-
 router.post('/', function(req, res, next) {
-
-    var value_name = req.body.name;  //retrieve the data associated with name
+    var value_name = req.body.name;
     res.send("hello " + value_name);
 });
-
 
 module.exports = router;
