f463c597ea724e50f92e6e9a0e2be4a96b4b0186
Steven <Sphan510@gmail.com>
2017-11-07 01:31:26 -0800

    versio

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+/*
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello World!' });
+});
+*/
+
 //########################################
-
 //to process data sent in on request need body-parser module
-
 var bodyParser = require('body-parser');
 var path = require ('path'); //to work with separtors on any OS including Windows
 var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
@@ -13,9 +18,25 @@ router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
 //#########################################
 
+
+
+// GET with  URI  /read/Lynne   which means name=Lynne
 router.post('/', function(req, res, next) {
-    var value_name = req.body.name;
+
+
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    //expecting data variable called name --retrieve value using body-parser
+
+    var value_name = req.body.name;  //retrieve the data associated with name
+
+    //var value_name = req.params.name;  //retrieve the data associated with name
+
+    //res.render('readNameAndRespond', {outputName: req.params.name })
+
     res.send("hello " + value_name);
 });
+
 
 module.exports = router;
