e72da8efcc5ab726eb2237c98d4905ef877daeb4
Steven <Sphan510@gmail.com>
2017-11-16 13:43:37 -0800

    version 2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var mongodb = require('mongodb');
+
 
 /* GET home page. */
 
@@ -61,19 +64,58 @@ router.post('/readOrderAndRespond', function(req, res, next) {
     var addr1 = req.body.addr1;
     var city = req.body.city;
 
+    //app.set('firstName', firstName);
+
     //var value_name = req.params.name;  //retrieve the data associated with name
 
-    res.render('readOrderAndRespond',
-        {firstName: firstName, lastName: lastName, addr1: addr1, city:city})
+    //res.render('readOrderAndRespond',
+        //{firstName: firstName, lastName: lastName, addr1: addr1, city:city})
 
 
+    / Create seed data -- it is in JSON format
+    var seedData = [
+        {
+            FirstName: firstName,
+            LastName: lastName,
+            Address: addr1,
+            City: city,
 
+        }
+    ];
 
+// Standard URI format:  mongodb://[dbuser:dbpassword@]host:port/dbname
+// GO TO mLab.com account to see what YOUR database URL is
+//CHANGE the url so it is correct for your account
+    var uri ='mongodb://steven:steven@ds259175.mlab.com:59175/songscs3520';
 
+//using mongodb module
+    mongodb.MongoClient.connect(uri, function(err, db) {
 
+        if(err) throw err;
+
+        /*
+         * First we'll add a  few songs. Nothing is required to create the
+         * songs collection;  it is created automatically when we insert.
+         */
+        var Customers =  db.collection('Customers');
+
+        // Note that the  insert method can take either an array or a dict.
+        Customers.insert(seedData, function(err, result) {
+            if(err) throw err;
+
+            /*
+             * Then we need to  give Boyz II Men credit for their contribution
+             * to the hit  "One Sweet Day".
+             */
+
+        });
+    });
 
 
 });
+
+
+//router.post('/insert')
 
 
 
--- routes/insert.js
+++ routes/insert.js
@@ -5,24 +5,15 @@ URI.
 */
 
 var mongodb = require('mongodb');
+var app = express();
 
 // Create seed data -- it is in JSON format
 var seedData = [
     {
-        name: 'Steven',
-        billing: '5555555',
-        shipping: 'Island',
+        FirstName: req.app.get('firstName')
+        //billing: '5555555',
+        //shipping: 'Island',
 
-    },
-    {
-        name: 'Debbie',
-        billing: '23213123',
-        shipping: 'LAND',
-    },
-    {
-        name: 'Steph',
-        billing: 'Yukon dr',
-        shipping: 'animas dr',
     }
 ];
 
@@ -40,27 +31,16 @@ mongodb.MongoClient.connect(uri, function(err, db) {
      * First we'll add a  few songs. Nothing is required to create the
      * songs collection;  it is created automatically when we insert.
      */
-    var Orders =  db.collection('Orders');
+    var Customers =  db.collection('Customers');
 
     // Note that the  insert method can take either an array or a dict.
-    Orders.insert(seedData, function(err, result) {
+    Customers.insert(seedData, function(err, result) {
         if(err) throw err;
 
         /*
          * Then we need to  give Boyz II Men credit for their contribution
          * to the hit  "One Sweet Day".
          */
-        Orders.update(
-            { name: 'Steven' },
-            { $set: {  billing: 'Mariah Carey ft. Boyz II Men' } },
-            function (err,  result) {
-                if(err) throw  err;
-                /*
-                 * Finally we  run a query which returns all the hits that spend 10 or
-                 * more weeks  at number 1.
-                 */
 
-            }
-        );
     });
 });
