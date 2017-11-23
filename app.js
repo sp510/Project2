7ebb0419aebf91ed350ccb482c770d6bc612d047
Steven <Sphan510@gmail.com>
2017-11-22 17:46:27 -0800

    version 2

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use ('/', storeData);
+app.use ('/sD', storeData);
 
 
 // catch 404 and forward to error handler
